a0843a714142af3501587034a18018e579a85954
Andrey Fadeev <me@ciiol.net>
2020-07-28 18:05:05 +0300

    DC-129 Allow to define more flexible conditions in provision terms (#602)

--- proto/domain.thrift
+++ proto/domain.thrift
@@ -2376,6 +2376,7 @@ struct ProvisionTermSet {
 }
 
 struct PaymentsProvisionTerms {
+    11: optional Predicate allow
     1: optional CurrencySelector currencies
     2: optional CategorySelector categories
     3: optional PaymentMethodSelector payment_methods
@@ -2436,6 +2437,7 @@ struct WalletProvisionTerms {
 }
 
 struct WithdrawalProvisionTerms {
+    5: optional Predicate allow
     1: optional CurrencySelector currencies
     2: optional PayoutMethodSelector payout_methods
     3: optional CashLimitSelector cash_limit
