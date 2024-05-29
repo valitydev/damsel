33633d45177fffa32903c7ddf4a3854b6666dfb2
Артем <WWW_cool@inbox.ru>
2024-05-29 13:32:36 +0300

    added destination auth data

--- proto/withdrawals_domain.thrift
+++ proto/withdrawals_domain.thrift
@@ -13,6 +13,16 @@ struct Withdrawal {
     2: required Destination destination
     3: optional Identity sender
     4: optional Identity receiver
+    6: optional AuthData auth_data
+}
+
+union AuthData {
+    1: SenderReceiverAuthData sender_receiver
+}
+
+struct SenderReceiverAuthData {
+    1: required string sender_token
+    2: required string receiver_token
 }
 
 union Destination {
