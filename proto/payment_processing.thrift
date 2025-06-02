5ca4f4a2af6bd68ba4d255889c0e2c0c1c5479a4
Артем <WWW_cool@inbox.ru>
2025-06-02 17:55:04 +0300

    Fix: compute terms in invoicing (#164)
    * removed party revision from method
    
    * same for invoice tmpl

--- proto/payment_processing.thrift
+++ proto/payment_processing.thrift
@@ -1076,7 +1076,6 @@ service Invoicing {
 
     domain.TermSet ComputeTerms (
         2: domain.InvoiceID id
-        3: PartyRevisionParam party_revision_param
     )
         throws (
             2: InvoiceNotFound ex2
@@ -1504,14 +1503,11 @@ service InvoiceTemplating {
     /* Terms */
 
     domain.TermSet ComputeTerms (
-        2: domain.InvoiceTemplateID id,
-        3: base.Timestamp timestamp,
-        4: PartyRevisionParam party_revision_param
+        2: domain.InvoiceTemplateID id
     )
         throws (
             2: InvoiceTemplateNotFound ex2,
-            3: InvoiceTemplateRemoved ex3,
-            4: PartyNotExistsYet ex4
+            3: InvoiceTemplateRemoved ex3
         )
 }
 
