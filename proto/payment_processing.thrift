f01f0e24cb470c957490917dc0c0aa0d3a056484
Артем <WWW_cool@inbox.ru>
2025-08-29 12:20:01 +0300

    revert refs

--- proto/payment_processing.thrift
+++ proto/payment_processing.thrift
@@ -1487,8 +1487,6 @@ service InvoiceTemplating {
 /* Party management service definitions */
 
 // Types
-
-typedef domain.WalletID WalletID
 typedef domain.PaymentInstitutionRef PaymentInstitutionRef
 
 struct Varset {
@@ -1496,8 +1494,8 @@ struct Varset {
     2: optional domain.CurrencyRef currency
     3: optional domain.Cash amount
     4: optional domain.PaymentMethodRef payment_method
-    5: optional domain.WalletID wallet_id
-    6: optional domain.ShopID shop_id
+    5: optional base.ID wallet_id
+    6: optional base.ID shop_id
     8: optional domain.PaymentTool payment_tool
     9: optional domain.PartyConfigRef party_ref
     10: optional domain.BinData bin_data
